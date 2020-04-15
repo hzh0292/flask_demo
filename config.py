a4f6740e4c596306d693fe19d4a5e4f08306c97d
hzh0292 <hzh0292@qq.com>
2020-04-15 12:20:44 +0800

    2020-4-15 12:20:02

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
+from _config import PASSWORD
 SECRET_KEY = "94853e5a-db7b-4e4f-b6c0-7e24f1beb261"
 DEBUG = True
-PASSWORD = ''
 SQLALCHEMY_DATABASE_URI = f'postgres://bwkeosnd:{PASSWORD}@john.db.elephantsql.com:5432/bwkeosnd'
 SQLALCHEMY_TRACK_MODIFICATION = False
