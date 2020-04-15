b16e58a00fbcc923ac22b812ef4244fae0608f2b
hzh0292 <hzh0292@qq.com>
2020-04-15 11:48:12 +0800

    2020-04-15 11:48:10

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 SECRET_KEY = "94853e5a-db7b-4e4f-b6c0-7e24f1beb261"
 DEBUG = True
-HOSTNAME = '127.0.0.1'
-PORT = '3306'
-DATABASE = 'zlktqa'
-USERNAME = 'root'
-PASSWORD = '123456'
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8'
-
+PASSWORD = ''
+SQLALCHEMY_DATABASE_URI = f'postgres://bwkeosnd:{PASSWORD}@john.db.elephantsql.com:5432/bwkeosnd'
 SQLALCHEMY_TRACK_MODIFICATION = False
